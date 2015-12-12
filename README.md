060f6f84fa63333b0d8ea679256952611a620256
JCharante <ralphcharante@gmail.com>
2015-12-11 20:59:03 -0500

    Updated readme, new data inserted with POST & JSON

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # duality
-The server software on the BPU
+The server software on the BPU (Base Prototype Unit)
+
+# Future Releases & Features
+
+### Citadel (Spring 2016)
+* Full API Documentation
+* Polished Data Visualizer
+* Finished & Polished Command Panel for the Hub
+
+### Galatea (Early 2016)
+* Multiple Real-Time Sensor Support
+* Beta Command Panel for the Hub
+* Beta Data Visualizer
+* Beta API Documentation
+* Beta Simultaneous Sensor Auto-Setup
+
+### Aegis (Late 2015)
+* Non-Simultaneous Sensor Auto-Setup
+* Alpha Data Visualizer
+* Alpha API Documentation
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
-import bcrypt, uuid, json, random
+import bcrypt, uuid, json, random, time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -21,10 +21,12 @@ class Misc(db.Model):
 
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    time = db.Column(db.String())
-    value = db.Column(db.String())
+    sensor_id = db.Column(db.Integer)
+    time = db.Column(db.Integer)
+    value = db.Column(db.Integer)
 
-    def __init__(self, time, value):
+    def __init__(self, sensor_id, time, value):
+        self.sensor_id = sensor_id
         self.time = time
         self.value = value
 
@@ -32,59 +34,30 @@ class Data(db.Model):
         return '<data %r>' % self.id
 
 
-def database_save_data(time, value):
-    db.session.add(Data(time, value))
-    db.session.commit()
-
-
 @app.route('/')
 def index():
     return render_template('client.html')
 
 
-@app.route('/add/<time>/<int:value>')
-def add(time, value):
-    data = json.loads(Misc.query.filter(Misc.id == 1).first().value)
-    data[str(time)] = value
-    data = json.dumps(data)
-    Misc.query.filter(Misc.id == 1).first().value = data
+@app.route('/add', methods=['POST'])
+def add_message():
+    content = request.get_json(silent=True)
+    last_entry = Data.query.filter(Data.sensor_id == 1).order_by(Data.id.desc()).first()
+    db.session.add(Data(content["ID"]["sensor_id"], last_entry.time + 1, content["Payload"]))
     db.session.commit()
-    response = json.loads(Misc.query.filter(Misc.id == 1).first().value)
-    return jsonify(**response)
-
-
-@app.route('/add_value/<int:value>')
-def add_value(value):
-    data = json.loads(Misc.query.filter(Misc.id == 1).first().value)
-    data[len(data) + 1] = value
-    data = json.dumps(data)
-    Misc.query.filter(Misc.id == 1).first().value = data
-    db.session.commit()
-    response = json.loads(Misc.query.filter(Misc.id == 1).first().value)
-    return jsonify(**response)
+    return "meow"
 
 
 @app.route('/jsondata')
 def json_data():
-    data = json.loads(Misc.query.filter(Misc.id == 1).first().value)
     arrays = []
-    for key, value in data.items():
-        arrays.append([int(key), value])
+    for r in Data.query.filter(Data.sensor_id == 1).all():
+        arrays.append([r.time, r.value])
     response = {
-        "label": "Duality Alpha",
+        "label": "Distance Sensor 1",
         "data": arrays
     }
     return jsonify(**response)
-
-
-@app.route('/wipe')
-def wipe():
-    data = {
-        '1': 1
-    }
-    Misc.query.filter(Misc.id == 1).first().value = json.dumps(data)
-    db.session.commit()
-    return "wiped"
 
 
 app.run(debug=True, host='0.0.0.0', port=8000)
