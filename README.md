0c04e88d323cd82de06e4777d5245a6a90317565
JCharante <ralphcharante@gmail.com>
2015-12-12 08:56:36 -0500

    Updated progress on readme, added the ability to choose which sensor feed to display. There's a bug which stops the data feed to stop updating when switching to a new feed.

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # duality
 The server software on the BPU (Base Prototype Unit)
 
+# Progress
+### Next Build: Aegis (22.5%)
+## Features:
+* Non-Simultaneous Sensor Auto-Setup (0%):
+    1. Sensor Identity Change Protocol: 0%
+* Alpha Data Visualizer (37.5%):
+    1. Display A Data Feed: 100%
+    2. Switch Between Data Feeds: 50%
+    3. Display Multiple Data Feeds Separately: 0%
+    4. Display Multiple Data Feeds Together: 0%
+* Alpha API Documentation (30%):
+    1. Sensor Data Transfer Protocol: 50%
+    2. Data Feed API: 10%
+
 # Future Releases & Features
 
 ### Citadel (Spring 2016)
--- app.py
+++ app.py
@@ -42,21 +42,33 @@ def index():
 @app.route('/add', methods=['POST'])
 def add_message():
     content = request.get_json(silent=True)
-    last_entry = Data.query.filter(Data.sensor_id == 1).order_by(Data.id.desc()).first()
+    last_entry = Data.query.filter(Data.sensor_id == content["ID"]["sensor_id"]).order_by(Data.id.desc()).first()
     db.session.add(Data(content["ID"]["sensor_id"], last_entry.time + 1, content["Payload"]))
     db.session.commit()
     return "meow"
 
 
-@app.route('/jsondata')
-def json_data():
+@app.route('/jsondata/<sensor_id>')
+def json_data(sensor_id):
     arrays = []
-    for r in Data.query.filter(Data.sensor_id == 1).all():
+    for r in Data.query.filter(Data.sensor_id == sensor_id).all():
         arrays.append([r.time, r.value])
     response = {
         "label": "Distance Sensor 1",
         "data": arrays
     }
+    return jsonify(**response)
+
+
+@app.route('/differentSensors')
+def different_sensors():
+    query = db.session.query(Data.sensor_id.distinct().label("sensor_id"))
+    sensors = [row.sensor_id for row in query.all()]
+    response = {
+
+    }
+    for sensor_id in sensors:
+        response[str(sensor_id)] = sensor_id
     return jsonify(**response)
 
 
